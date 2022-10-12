49c5e839aedf4dc90915117921f9787014ed62b0
Azeem <azeem2005@outlook.com>
2022-10-12 11:11:35 +0200

    lagring

--- Azeem_monster.py
+++ Azeem_monster.py
@@ -1,2 +1,34 @@
+import random
+import os
+import sys, time
 
-#test
+def slow_type(t):
+    typing_speed = 50 #wpm
+    for l in t:
+        sys.stdout.write(l)
+        sys.stdout.flush()
+        time.sleep(random.random()*10.0/typing_speed)
+
+
+
+class Spiller:
+    def __init__(self,navn,helse,styrke):
+        self.navn= navn
+        self.helse= helse
+        self.styrke= styrke
+        
+
+class Monster:
+    def __init__(self,navn,helse,styrke):
+        self.navn= navn
+        self.helse= helse
+        self.styrke= styrke
+
+monster1=Monster("Abdi",150,30)
+
+spiller1=Spiller("Azeem",100,50)
+
+slow_type("Velkommen til Arena 1v1 \n")
+
+while (True):
+    break
