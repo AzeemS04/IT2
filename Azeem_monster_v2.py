75b500ac0b8972f32c5b7583a079dc3f33bb56b9
Azeem <azeem2005@outlook.com>
2022-10-13 13:19:26 +0200

    save

--- Azeem_monster_v2.py
+++ Azeem_monster_v2.py
@@ -18,9 +18,7 @@ class Player:
         self.strength=strength
         self.wallet=wallet
 
-spiller = Player("Azeem",100,10)
 
-print(spiller.wallet)
 
 class Monster:
     def __init__(self,name,health,strength,reward):
@@ -32,4 +30,41 @@ class Monster:
 monster1=Monster("Devil",80,20,15)
 "lage en metode inne i Monster klassen som lager monster random"
 
+
+def spill():
+    type(f'Velkommen til spillet {spiller.name}\n')
+    type("Hva vil du gjøre? \n")
+    type("Vil du utforske[utforske], sjekke lommene dine[inventory] eller kjøpe noe fra butikken[butikk]\n")
+    user_choice= input(": ")
+    valg=["utforske","inventory","butikk"]
+    while user_choice not in valg:
+        user_choice= input(": ")
+        if user_choice=="utforske":
+            type("Du tar en spaser tur\n")
+            hendelse= random.randint(1,1) #Kan legge til flere hendelser
+            if hendelse == 1:
+                fight()
+        elif user_choice=="inventory":
+            inventory()
+        elif user_choice=="butikk":
+            butikk()
+        else:
+            type("Skriv et gyldig valg\n")
+            type("Vil du utforske[utforske], sjekke lommene dine[inventory] eller kjøpe noe fra butikken[butikk]\n")
+
+
+
+
+
+
+
+
+
+type("Velkommen til spillet \n")
+type("Hva heter du")
+navn_input=input(": ")
+spiller=Player(navn_input,100,10)
+
+while (True):
+    spill()
 
